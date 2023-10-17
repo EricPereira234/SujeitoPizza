3aa24499604138e4cd9bd419d69559a095aaa4ee
Eric Pereira <ericpereira234@gmail.com>
2023-10-17 07:56:09 -0300

    criando controlles

--- backend/src/route.ts
+++ backend/src/route.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from "express";
 
+import { CreateUserController } from "./controllers/user/CreateUserController";
+
 const router = Router();
 
 
@@ -7,6 +9,8 @@ const router = Router();
 router.get('/teste', (req: Request, res: Response)=>{
     throw new Error('Erro 404');
 });
+
+router.post('/users', new CreateUserController().handle)
 
 
 export {router};

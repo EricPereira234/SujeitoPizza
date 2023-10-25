f94f969c6e1f0631b77954d5573fc97f72bc5ccb
Eric Pereira <ericpereira234@gmail.com>
2023-10-25 07:55:07 -0300

    criando controllers

--- backend/src/controllers/services/user/CreateUserServices.ts
+++ backend/src/controllers/services/user/CreateUserServices.ts
@@ -1,3 +1,7 @@
+import prismaClient from '../../../prisma';
+import { hash } from 'bcryptjs';
+
+
 interface UserRequest {
     name: string;
     email: string;
@@ -6,8 +10,42 @@ interface UserRequest {
 
 class CreateUserService{
     async execute({name, email, password}: UserRequest){
+
+        //verificando se ele enviou um email
+        if(!email){
+            throw new Error("Esse email incorrect");
+        }
+
+        //verifica se esse email já está cadastrado na plataforma
+        const userAlereadyExists = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+        if(userAlereadyExists){
+            throw new Error ('esse usuário já existe')
+        }
+
+
+        //criptografando senha com o bcryptjs antes de salvar 
+        const passwordHash = await hash(password, 8);
+
+        //cadastrando usuario no banco
+        const user = await prismaClient.user.create({
+            data: {
+                name: name,
+                email: email,
+                password: passwordHash,
+            },
+
+            select: {
+                id: true,
+                name: true,
+                email: true,
+            }
+        })
     
-        return {name: name}
+        return user;
     }
 }
 

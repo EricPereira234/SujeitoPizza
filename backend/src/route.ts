29b4c04df3b98d02110b73ae13aaef9f1bfd2328
Eric Pereira <ericpereira234@gmail.com>
2023-11-02 10:43:41 -0300

    salvando categorias

--- backend/src/route.ts
+++ backend/src/route.ts
@@ -3,6 +3,7 @@ import { Router, Request, Response } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 
 import { isAuthenticated } from "./middleware/isAuthenticated";
 
@@ -19,6 +20,12 @@ router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 
 router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+
+// rota category
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+
+
 
 
 export {router};

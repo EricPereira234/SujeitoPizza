92b7b0fb03d78c2e62997ccefc41d7cbdad3108e
Eric Pereira <ericpereira234@gmail.com>
2023-11-06 07:56:09 -0300

    salvando imagens

--- backend/src/controllers/product/CreateProductController.ts
+++ backend/src/controllers/product/CreateProductController.ts
@@ -3,9 +3,33 @@ import { CreateProductService } from "../services/product/CreateProductService";
 
 
 class CreateProductController {
-    async handle(){
+    async handle(req: Request, res: Response) {
+        const { name, price, description, category_id } = req.body;
 
+        const createProductService = new CreateProductService();
+
+        if (!req.file) {
+
+            throw new Error("error upload file")
+
+        } else {
+            const {originalname, filename: banner} = req.file;
+
+
+            const product = await createProductService.execute({
+                name,
+                price,
+                description,
+                banner,
+                category_id
+            });
+
+
+            return res.json(product);
+
+        }
     }
+
 }
 
-export {CreateProductController}
+export { CreateProductController }
--- backend/src/route.ts
+++ backend/src/route.ts
@@ -1,14 +1,20 @@
 import { Router, Request, Response } from "express";
+import multer from "multer";
 
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
 
 import { isAuthenticated } from "./middleware/isAuthenticated";
+import uploadConfig from "./config/multer";
 
 const router = Router();
+
+// configurando envio de imagens
+const upload = multer(uploadConfig.upload("./tmp"))
 
 
 //rotas
@@ -26,6 +32,9 @@ router.get('/me', isAuthenticated, new DetailUserController().handle)
 // rota category
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
+// rota produtos
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
 
 
 

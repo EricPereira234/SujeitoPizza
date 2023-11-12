d2f4885e7d291c54deb8cb1504e71e75524f3f95
Eric Pereira <ericpereira234@gmail.com>
2023-11-12 09:53:22 -0300

    listando os envios dos pedidos

--- backend/src/route.ts
+++ backend/src/route.ts
@@ -19,6 +19,7 @@ import { RemoveItemController } from "./controllers/order/RemoveItemController";
 import { SendOrderController } from "./controllers/order/SendOrderController";
 import { ListOrderController } from "./controllers/order/ListOrderController";
 import { DatailOrderController } from "./controllers/order/DatailOrderController";
+import { FinishOrderController } from "./controllers/order/FinishOrderController";
 
 import { isAuthenticated } from "./middleware/isAuthenticated";
 import uploadConfig from "./config/multer";
@@ -58,6 +59,7 @@ router.delete('/order/remove', isAuthenticated, new RemoveItemController().handl
 router.put('/order/send', isAuthenticated, new SendOrderController().handle)
 router.get('/orders', isAuthenticated, new ListOrderController().handle)
 router.get('/order/datail', isAuthenticated, new DatailOrderController().handle)
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle)
 
 
 
